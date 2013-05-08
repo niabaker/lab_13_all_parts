20f49909549f1e7d744f7947d2920a7e105df6b5
Student <student@ent-v321-007.local>
2013-05-08 19:37:31 -0400

    lyric done

---
+++ lyrics.py
@@ -0,0 +1,32 @@
+"""
+lyrics.py
+=====
+* write three programs that all do the same thing!
+* all of these programs should:
+  * print out the same four lines from your favorite song
+  * each line of lyrics should be printed out on a new line
+  * for example, if your program prints out the song, 'On a Tropical Island', 
+    the output would look like (of course, with three version, imagine this
+    three times): 
+
+On a tropical island,
+Underneath a molten lava moon.
+Hangin' with the hula dancers,
+Askin' questions cause' they got all the answers.
+
+* there are multiple ways of printing out strings on new lines:
+  * maybe it's just one command
+  * or several commands
+  * or use different ways of representing strings, putting strings 
+    together, or incorporating newlines
+* write THREE DIFFERENT versions of this program in this file 
+"""
+
+print ("Imma buff baby that can dance like a man")
+print ("I can shake-a mah fanny, I can shak-a mah can")
+print ("Imma tuff tootin baby I can punch on yo buns")
+
+print("Imma buff baby that can dance like a man\nI can shake-a mah fanny, I can shake-a my can\nImma tough tootin baby I can punch on yo buns")
+
+print("Imma buff baby that can dance like a man\n" + "I can shake-a mah fanny, I can shake-a mah can\n" + "Imma tough tootin baby, I can punch on yo buns/\n")
+
